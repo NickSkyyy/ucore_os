f206b6415648b214eea8b829631c95434bb7b151
NickSkyyy <1755987589@qq.com>
2020-11-09 16:54:33 +0800

    a?

--- labcodes/lab2/kern/mm/buddy_system.c
+++ labcodes/lab2/kern/mm/buddy_system.c
@@ -9,7 +9,7 @@ free_area_t free_area[MAX_LEVEL + 1];
 static void
 bds_init(void) {
     for (int i = 0; i <= MAX_LEVEL; i++) {
-        list_init(free_area + i);
+        list_init(&(free_area[i].free_list));
         free_area[i].nr_free = 0;
     }
 }
@@ -25,4 +25,17 @@ bds_inti_memmap(struct Page *base, size_t n) {
         set_page_ref(p, 0);
     }
     // save as buddy    
+    p = base;
+    size_t temp = n;        // uesd to calculate 
+    int level = MAX_LEVEL;  // 12 as default
+    while (level >= 0) {
+        for (int i = 0; i < temp / (1 << level); i++) {
+            p->property = level;
+            SetPageProperty(p);
+            list_add_before(&(free_area[level].free_list), &(p->page_link));
+            p += (1 << level);
+        }
+        temp = temp % (1 << level);
+        level--;
+    }
 }
